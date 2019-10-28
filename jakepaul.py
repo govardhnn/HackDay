6aef65144576c46af16311716e57049072425c16
SrinjayMaitra <57004863+SrinjayMaitra@users.noreply.github.com>
2019-10-28 10:52:08 +0530

    Update jakepaul.py
    U are an absolute legend.Undoubtedly

--- jakepaul.py
+++ jakepaul.py
@@ -1,3 +1,13 @@
-print("hi")
-lame=print
-lame("hi")
+#Its everyday bro
+def paul(jake):
+    if(jake<=500000):
+        print("Five million subs in six months never done before")
+    elif(jake>500000):
+        print("Pewdiepie is next")
+    else:
+        print("At this moment jake realised he f***ed up")
+    return(jake)
+ass=int(input("Eneter no. of times u want to kick dat ass")) 
+paul(ass)   
+#Its everyday bro with the disney channel flow,five million subs in six months never done before!!got the brand new
+rolex,pewdiepie is next.
