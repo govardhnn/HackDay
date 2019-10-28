c54dfceba286dcd8eb7f98150c3dbc2bfb53884d
Srinivas-VJ <56502757+Srinivas-VJ@users.noreply.github.com>
2019-10-28 10:35:00 +0530

    made towerofhanoi problem
    meaaap

--- try.py
+++ try.py
@@ -1,3 +1,11 @@
-print("Hello")
-a=int(input("Enter any value"))
-print(a)
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
+    if n == 1: 
+        print ("Move disk 1 from rod",from_rod,"to rod",to_rod) 
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
+    print ("Move disk",n,"from rod",from_rod,"to rod",to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
+          
+# Driver code 
+n = 4
+TowerOfHanoi(n, 'A', 'C', 'B') 
